0041f10bec463874077de0b986e426b379442601
dhaupin <dhaupin@gmail.com>
2016-01-05 14:57:26 -0500

    Added sandbox mode string, renamed title

--- catalog/language/english/payment/simple_braintree_payments.php
+++ catalog/language/english/payment/simple_braintree_payments.php
@@ -1,12 +1,13 @@
 <?php
 // Text
-$_['text_title']           = 'Pay Securely with Credit / Debit Card (Braintree)';
+$_['text_title']           = 'Pay With Credit Card or PayPal (Secure)';
 $_['text_credit_card']     = 'Credit / Debit Card Details';
 $_['text_wait']            = 'Please wait!';
+$_['text_test_msg']        = 'Gateway is running in Sandbox mode. This transaction will not be charged. Visa Test: 4009348888881881';
 
 // Entry
 $_['entry_cc_owner']       = 'Card Owner:';
 $_['entry_cc_number']      = 'Card Number:';
-$_['entry_cc_expire_date'] = 'Card Expiry Date:';
+$_['entry_cc_expire_date'] = 'Card Expiry Date (MM/YY):';
 $_['entry_cc_cvv2']        = 'Card Security Code (CVV2):';
 ?>
